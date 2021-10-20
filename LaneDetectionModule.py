a9b7090982916c27b113cba02992f05ba68ef3bf
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-10-20 00:05:55 -0500

    hi

--- LaneDetectionModule.py
+++ LaneDetectionModule.py
@@ -3,6 +3,7 @@ import numpy as np
 import utlis
 
 def getLaneCurve(img):
+    imgCopy = img.copy()
 
     ####STEP 1
     imgThres=utlis.thresholding(img)
@@ -10,15 +11,35 @@ def getLaneCurve(img):
     ####STEP 2
     h,w,c =  img.shape
 
-    #imgWarp=utlis.warpImg(img, points, w,h)
+    points = utlis.valTrackBars()
+    imgWarp=utlis.warpImg(img, points, w,h)
+    imgWarpPoints = utlis.drawPoints(img, points)
 
+    imgThres = cv2.resize(imgThres, (480, 240))
+    imgWarp = cv2.resize(imgWarp, (480, 240))
+    imgWarpPoints = cv2.resize(imgWarpPoints, (480, 240))
+
+    #### STEP 3
+    basePoint, imgHist =  utlis.getHistogram(imgWarp, display=True)
 
     cv2.imshow("Thres", imgThres)
+    cv2.imshow("Warp", imgWarp)
+    cv2.imshow("Warp Points", imgWarpPoints)
+    cv2.imshow("Histogram", imgHist)
     return None
 
 if __name__=='__main__':
-    cap =cv2.VideoCapture('video')
+    cap =cv2.VideoCapture('vid1.mp4')
+    initialTrackBarsVals = [96,667,162,100]
+    utlis.initializeTrackbars(initialTrackBarsVals)
+    frameCounter = 0
     while True:
+        frameCounter+=1
+        if cap.get(cv2.CAP_PROP_FRAME_COUNT) == frameCounter:
+            cap.set((cv2.CAP_PROP_POS_FRAMES), 0)
+            frameCounter = 0
+
+
         success, img=cap.read()
         getLaneCurve(img)
 
--- utlis.py
+++ utlis.py
@@ -3,14 +3,14 @@ import numpy as np
 
 def thresholding(img):
     imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lowerWhote = np.array([80,0,0])
+    lowerWhote = np.array([75,0,0])
     upperWhite = np.array([255,160,255])
     maskWhite = cv2.inRange(imgHsv, lowerWhote, upperWhite)
     return maskWhite
 
 def warpImg(img, points, w, h):
     pts1 = np.float32(points)
-    pts2=np.float([[0,0], [w,0],[0,h],[w,h]])
+    pts2 = np.float32([[0,0], [w,0],[0,h],[w,h]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     imgWarp = cv2.warpPerspective(img, matrix, (w,h))
     return imgWarp
@@ -18,12 +18,43 @@ def warpImg(img, points, w, h):
 def nothing(a):
     pass
 
-def initializeTrackbars(initialTrackbarVals, wT=480, hT=240):
+def initializeTrackbars(initialTrackbarVals, wT=1080, hT=720):
     cv2.namedWindow("Trackbars")
     cv2.resizeWindow("Trackbars", 360, 240)
     cv2.createTrackbar("Width Top", "Trackbars", initialTrackbarVals[0], wT//2, nothing)
-    cv2.createTrackbar("Height Top", "Trackbars", initialTrackbarVals[1], hT // 2, nothing)
-    cv2.createTrackbar("Width Bottom", "Trackbars", initialTrackbarVals[0], wT // 2, nothing)
-    cv2.createTrackbar("Width Top", "Trackbars", initialTrackbarVals[0], wT // 2, nothing)
+    cv2.createTrackbar("Height Top", "Trackbars", initialTrackbarVals[1], hT , nothing)
+    cv2.createTrackbar("Width Bottom", "Trackbars", initialTrackbarVals[2], wT // 2, nothing)
+    cv2.createTrackbar("Height Bottom", "Trackbars", initialTrackbarVals[3], hT, nothing)
 
+def valTrackBars(wT=1080, hT=720):
+    widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
+    heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
+    widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
+    heightBottom = cv2.getTrackbarPos("Height Bottom", "Trackbars")
+    points = np.float32([(widthTop, heightTop), (wT-widthTop , heightTop), (widthBottom, heightBottom), (wT-widthBottom, heightBottom)])
+    return  points
 
+def drawPoints(img, points):
+    for x in range(4):
+        cv2.circle(img, (int(points[x][0]), int (points[x][1])), 15, (0,0,255), cv2.FILLED)
+    return img
+
+def getHistogram(img, minPier=0.1, display=False):
+    histValues = np.sum(img, axis=0)
+    #print(histValues)
+    maxValue = np.max(histValues)
+    minValue = np.min(histValues)
+
+    indexArray = np.where(histValues>=minValue)
+    basePoint = int(np.average(indexArray))
+    print(basePoint)
+
+    if display:
+        imgHist = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+        for x, intensity in enumerate(histValues):
+            print((x,img.shape[0]))
+            print(x, intensity[0]/255)
+            cv2.line(imgHist, (x,img.shape[0]), (x, img.shape[0] - int(intensity[0]/255)), (255,0,255), 1)
+            cv2.circle(imgHist, (basePoint, img.shape[0]), 20, (0,255,255), cv2.FILLED)
+        return basePoint, imgHist
+    return basePoint
